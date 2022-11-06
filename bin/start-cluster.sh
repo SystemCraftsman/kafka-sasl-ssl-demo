3f583fa48f314392756739fe667adb7f6d33c73f
mabulgu <abulgu@redhat.com>
2022-11-07 02:06:33 +0300

    add sleep

--- bin/start-cluster.sh
+++ bin/start-cluster.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./kafka/bin/zookeeper-server-start.sh -daemon ./resources/config/zookeeper.properties;
+./kafka/bin/zookeeper-server-start.sh -daemon ./resources/config/zookeeper.properties
+
+sleep 3000
 
 ./kafka/bin/kafka-server-start.sh -daemon ./$1/resources/config/server$2.properties
