db28215df46021ac48de1962f63e76475db9cf07
mabulgu <abulgu@redhat.com>
2022-11-07 02:07:39 +0300

    fix  sleep

--- bin/start-cluster.sh
+++ bin/start-cluster.sh
@@ -2,6 +2,6 @@
 
 ./kafka/bin/zookeeper-server-start.sh -daemon ./resources/config/zookeeper.properties
 
-sleep 3000
+sleep 5
 
 ./kafka/bin/kafka-server-start.sh -daemon ./$1/resources/config/server$2.properties
